d923c9701bee71eaf8c1c713836b850717e8af57
Subhani Shaik <118685031+subhanihw@users.noreply.github.com>
2023-01-10 13:12:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var arrLength = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<arrLength; i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click", function()
+  {
+    makeSound(this.innerHTML);
+    animation(this.innerHTML);
+  });
+}
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key.toLowerCase());
+  animation(event.key.toLowerCase());
+});
+
+function makeSound(key)
+{
+  switch(key)
+  {
+    case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+    case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+    case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+    case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+    case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+    case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+    case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+    default:console.log(thisHtml);
+  }
+}
+
+function animation(key) {
+  var currentKey = document.querySelector("."+key);
+  currentKey.classList.add("pressed");
+  setTimeout(function(){currentKey.classList.remove("pressed");}, 300);
+}
